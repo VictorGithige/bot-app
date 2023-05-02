467ed06ac82083dc9bd889872c6d32df5e535935
VictorGithige <muhuthiagithige1@gmail.com>
2023-05-02 20:04:18 +0300

    last commit

---
+++ Bots-master/src/App.js
@@ -0,0 +1,15 @@
+import BotsContainer from './BotsContainer';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <BotsContainer />
+    </div>
+  );
+}
+
+export default App;
